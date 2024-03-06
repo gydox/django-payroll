f3f1fe9e2d9c9d303353a32d243783bd1d048de6
Shawn <shawntayws@gmail.com>
2024-03-06 20:48:00 +0800

    create base template

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request=request,
-                  template_name="dashboard.html",
+                  template_name="main/dashboard.html",
                   context={
                   })
