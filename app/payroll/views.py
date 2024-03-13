e5d8584436247f8973323493d865225b59735f6a
Shawn <shawntayws@gmail.com>
2024-03-13 10:24:56 +0800

    creation of payroll will generate payroll items

--- app/payroll/views.py
+++ app/payroll/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from employee.models import Employee, FullTimeEmployee, PartTimeEmployee
 from .models import Payroll, PayrollItem, IncomeItem, Earnings, Deductions, EarningAllocation, DeductionAllocation, EarningAllocationArchive, DeductionAllocationArchive
 from django.contrib import messages
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 @login_required
 def payroll(request):
@@ -36,3 +38,14 @@ def payslip(request):
                   template_name="payroll/payslip.html",
                   context={
                   })
+
+@receiver(post_save, sender=Payroll)
+def create_payroll_items(sender, instance, created, **kwargs):
+    """
+    Signal receiver function to create PayrollItem instances for active employees
+    when a Payroll is created.
+    """
+    if created:
+        active_employees = Employee.objects.filter(active=True)
+        for employee in active_employees:
+            PayrollItem.objects.create(payroll=instance, employee=employee, amount=0.0)
